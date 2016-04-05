6f89bb01a095c91806abb357f531f2bffe51e6d8
StephAO <stephanearocaouellette@gmail.com>
2016-04-05 16:02:08 -0400

    Added different sizes to test

--- test.py
+++ test.py
@@ -13,13 +13,59 @@ def main():
     if len(sys.argv) == 2:
         mode = sys.argv[1]
 
-    mine_count = open('mineData.csv', 'w', newline='')
-    prob_succ = open('probData.csv', 'w', newline='')
+    # Beginner
+    mine_count = open('mineDataBeginner.csv', 'w', newline='')
+    prob_succ = open('probDataBeginner.csv', 'w', newline='')
     mc_writer = csv.writer(mine_count, quoting=csv.QUOTE_MINIMAL)
     ps_writer = csv.writer(prob_succ,  quoting=csv.QUOTE_MINIMAL)
     for i in range(NUMBER_OF_TEST):
 
-        b = Board.Board(16,30,99) # [(1,1),(2,2),(3,4),(2,4)]
+        b = Board.Board(9,9,10)
+        solve = MineSweeperCSP.MineSweeperCSP()
+        solve.set_mode(mode)
+        solve.run_all(b,mc_writer, ps_writer)
+
+    mine_count.close()
+    prob_succ.close()
+
+    # Intermediate
+    mine_count = open('mineDataIntermediate.csv', 'w', newline='')
+    prob_succ = open('probDataIntermediate.csv', 'w', newline='')
+    mc_writer = csv.writer(mine_count, quoting=csv.QUOTE_MINIMAL)
+    ps_writer = csv.writer(prob_succ,  quoting=csv.QUOTE_MINIMAL)
+    for i in range(NUMBER_OF_TEST):
+
+        b = Board.Board(16,16,40)
+        solve = MineSweeperCSP.MineSweeperCSP()
+        solve.set_mode(mode)
+        solve.run_all(b,mc_writer, ps_writer)
+
+    mine_count.close()
+    prob_succ.close()
+
+    # Expert
+    mine_count = open('mineDataExpert.csv', 'w', newline='')
+    prob_succ = open('probDataExper.csv', 'w', newline='')
+    mc_writer = csv.writer(mine_count, quoting=csv.QUOTE_MINIMAL)
+    ps_writer = csv.writer(prob_succ,  quoting=csv.QUOTE_MINIMAL)
+    for i in range(NUMBER_OF_TEST):
+
+        b = Board.Board(16,30,99)
+        solve = MineSweeperCSP.MineSweeperCSP()
+        solve.set_mode(mode)
+        solve.run_all(b,mc_writer, ps_writer)
+
+    mine_count.close()
+    prob_succ.close()
+
+    # Custom
+    mine_count = open('mineDataCustom.csv', 'w', newline='')
+    prob_succ = open('probDataCustom.csv', 'w', newline='')
+    mc_writer = csv.writer(mine_count, quoting=csv.QUOTE_MINIMAL)
+    ps_writer = csv.writer(prob_succ,  quoting=csv.QUOTE_MINIMAL)
+    for i in range(NUMBER_OF_TEST):
+
+        b = Board.Board(50,50,500)
         solve = MineSweeperCSP.MineSweeperCSP()
         solve.set_mode(mode)
         solve.run_all(b,mc_writer, ps_writer)
